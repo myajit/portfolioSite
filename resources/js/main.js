d0134cccc5c32e4e7f62d0b5ab9f78da626f8466
myajit <myajit@naver.com>
2022-03-08 01:09:11 +0900

    mod2

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,11 @@
 $('#switchImg').click(()=> {
-    location.href = 'http://jhj910328.ga/'
+    location.href = 'https://github.com/minjeeleee/final-switch'
+})
+
+$('#putsalImg').click(()=> {
+    location.href = 'https://github.com/minjeeleee/futsalon-boot'
+})
+
+$('#commentImg').click(()=> {
+    location.href = 'https://github.com/myajit/commentsite/tree/master'
 })
